c7466d215228224066b5110722921ec1c502f793
Himanshu Kumar <ht514440@gmail.com>
2023-12-14 22:23:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+
 import pickle
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -7,42 +8,35 @@ import secrets
 
 app = Flask(__name__)
 
-
-#Generate a secure random key
+# Generate a secure random key
 secret_key = secrets.token_hex(16)
 app.secret_key = secret_key
 
+# Import ridge regressor model and standard scaler pickle
+ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
 
-
-## import ridge regressor model and standard scaler pickle
-ridge_model=pickle.load(open('models/ridge.pkl','rb'))
-standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
-
-## Route for home page
+# Route for home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predict_datapoint', methods=['POST'])
 def predict_datapoint():
-    if request.method=='POST':
-        Temperature=float(request.form.get('Temperature'))
-        RH=float(request.form.get('RH'))
-        Ws=float(request.form.get('Ws'))
-        Rain=float(request.form.get('Rain'))
-        FFMC=float(request.form.get('FFMC'))
-        DMC=float(request.form.get('DMC'))
-        ISI=float(request.form.get('ISI'))
-        Classes=float(request.form.get('Classes'))
-        Region=float(request.form.get('Region'))
+    if request.method == 'POST':
+        # Extract input data from the form
+        # Your input processing logic here
 
-        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        result=ridge_model.predict(new_data_scaled)
+        # Scale the input data using the standard scaler
+        new_data_scaled = standard_scaler.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]])
 
-        return render_template('index.html',result=result[0])
+        # Predict using the ridge model
+        result = ridge_model.predict(new_data_scaled)
+
+        return render_template('index.html', result=result[0])
 
     else:
-        return render_template("index.html")
-        
-if __name__=="__main__":
-    app.run()
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
